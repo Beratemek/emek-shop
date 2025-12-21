ca5181dc002e550ab3ac4f75a6eea7fd4253e170
Beratemek <emekberat19@gmail.com>
2025-12-21 20:20:12 +0300

    Configure production CORS and API URL

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://emek-shop.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
